4df155d6afa49f0081def0ecc2201864d2802bdc
UltraLuigi <noam@goldwasserfamily.org>
2026-02-24 17:06:39 -0500

    fixed backend import issue

--- backend/app.py
+++ backend/app.py
@@ -2,23 +2,23 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
-from models import Transaction
-
 app = Flask(__name__)
 CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
+import models
+
 @app.route("/transactions")
 def get_transactions():
-    transactions = db.session.execute(db.select(Transaction)).all()
+    transactions = db.session.execute(db.select(models.Transaction)).all()
     return jsonify([dict(transaction) for transaction in transactions]) # check if can remove jsonify and just return list
 
 @app.route("/transactions", methods=["POST"])
 def add_transaction():
     data = request.get_json()
-    new_transaction = Transaction(
+    new_transaction = models.Transaction(
         description=data["description"],
         amount=data["amount"],
         category=data["category"]
@@ -29,7 +29,7 @@ def add_transaction():
 
 @app.route("/transactions/<int:id>", methods=["DELETE"])
 def delete_transaction(id):
-    transaction = db.session.get_or_404(Transaction, id)
+    transaction = db.session.get_or_404(models.Transaction, id)
     db.session.delete(transaction)
     db.session.commit()
     return jsonify({"message": "Transaction deleted successfully"}), 200
